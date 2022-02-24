2d31dc6415e7f46721b5547f01124a6c61d7d483
navabhi22 <navabhi22@gmail.com>
2022-02-24 21:31:02 +0530

    Added class for Checking the equality of lines

---
+++ CheckIfLinesAreEqual.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class CheckIfLinesAreEqual
+    {
+        /// <summary>
+        /// Equalities the of line.
+        /// </summary>
+        /// <returns></returns>
+        public static double Line1;
+        public static double Line2;
+        public bool EqualityOfLine()
+        {
+            CreatingLine line = new CreatingLine();
+            Line1 = line.CreateLine();
+            Line2 = line.CreateLine();
+            if (Line1 == Line2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
